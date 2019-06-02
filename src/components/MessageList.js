fe41b586b1f21ff1740f6e10fdd329a5c2eaefe4
Wesley Ferguson <pwesferguson@gmail.com>
2019-06-02 16:04:30 -0700

    add messagelist component

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,6 +4,20 @@ import Message from './Message'
 
 const MessageList = ({ messages, onMessageClick }) => (
     <ul className = "message-list">
-        
+        messages.map( (message, index) => {
+            <Message key={index} {...message} onClick={() => onMessageClick(index)} />
+        })
     </ul>
 )
+
+MessageList.PropTypes = {
+    messages: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired,
+    onMessageClick: PropTypes.func.isRequired
+}
+
+export default MessageList;
