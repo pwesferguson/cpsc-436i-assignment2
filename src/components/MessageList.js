772eda4465cbd04fe8c7adef61e50f3486a79c16
Wesley Ferguson <pwesferguson@gmail.com>
2019-06-03 21:49:56 -0700

    save changes

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Message from './Message'
-import { dispatch } from 'react-redux';
 
 const MessageList = ({ messages, displayMessageDetails }) => (
     <ul id = "shout-box-list">
