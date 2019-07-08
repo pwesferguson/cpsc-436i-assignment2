a0d0ac72a59a9a5e3daced306eed95608e7b2a01
Wes Ferguson <pwesferguson@gmail.com>
2019-07-08 03:17:34 -0700

    add procfile

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const store = createStore(shoutApp);
 // store.subscribe( () => console.log(store.getState()));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
